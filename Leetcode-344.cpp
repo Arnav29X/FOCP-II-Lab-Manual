04c82f24aa97452d933b05571f39ab5bb28f3365
Arnav Verma <94759225+Arnav29X@users.noreply.github.com>
2026-02-07 16:49:13 +0530

    Replace swap with manual character swapping

--- Leetcode-344.cpp
+++ Leetcode-344.cpp
@@ -5,7 +5,9 @@ public:
         int j = s.size() - 1;
 
         while(i < j) {
-            swap(s[i], s[j]);
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
             i++;
             j--;
         }
