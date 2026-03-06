9955fa2c5a9625c4295d587c007281692cd130aa
Arnav Verma <94759225+Arnav29X@users.noreply.github.com>
2026-03-06 20:44:52 +0530

    Implement program to sum two cash amounts

---
+++ sum-of-2.cpp
@@ -0,0 +1,17 @@
+// CASHIER WANTS TO SUM 2 AMOUNTS
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    float cash1, cash2;
+
+    cout << "Enter amount number 1: ";
+    cin >> cash1;
+    cout << "Enter amount number 2: ";
+    cin >> cash2;
+
+    cout << "\nSum of Amount 1st & Amount 2nd is: " << (float)(cash1 + cash2) << endl;
+
+    return 0;
+}
