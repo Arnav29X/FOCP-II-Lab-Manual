0048eb78e19e3c3a8d655d05157451ec35cad5bc
Arnav Verma <94759225+Arnav29X@users.noreply.github.com>
2026-03-06 20:44:22 +0530

    Add simple interest calculation program

---
+++ simple-interest.cpp
@@ -0,0 +1,16 @@
+// TO CALCULATE SIMPLE INTEREST
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    float P, R, T, SI;
+
+    cout << "Enter Principal, Rate, Time: ";
+    cin >> P >> R >> T;
+
+    SI = (P * R * T) / 100;
+    cout << "Simple Interest = " << SI << endl;
+
+    return 0;
+}
